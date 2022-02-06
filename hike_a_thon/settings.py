cdde4ecab780f8a206c125a2262bfc2f5bfd61e1
Alex Ciobanu <tsopi10@gmail.com>
2022-02-06 18:21:08 +0200

    Create basic admin view and add summernote

--- hike_a_thon/settings.py
+++ hike_a_thon/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hike-a-thon.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['hike-a-thon.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'trips',
 ]
 
--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Trip
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Trip)
+class TripAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
