e31d995d60859f4da3c7f7741ac5485a087f352b
Alex Ciobanu <tsopi10@gmail.com>
2022-02-23 11:17:22 +0200

    Style cards and add pagination

--- trips/views.py
+++ trips/views.py
@@ -10,7 +10,7 @@ class TripList(generic.ListView):
     model = Trip
     queryset = Trip.objects.order_by('date_start')
     template_name = 'index.html'
-    paginate_by = 8
+    paginate_by = 4
 
 
 class TripDetail(View):
