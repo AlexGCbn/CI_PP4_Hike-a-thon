565b553e2e2661e62a0edb1cdc827b94508c5065
Alex Ciobanu <tsopi10@gmail.com>
2022-03-14 12:50:04 +0200

    Add trip request URL and connect pages

--- trips/urls.py
+++ trips/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.TripList.as_view(), name='home'),  # Home URL
     path('dashboard/', views.TripsRegistered.as_view(), name='user_dashboard'),  # Trips history / user dashboard URL
+    path('dashboard/request/', views.TripRequest.as_view(), name='trip_request'),  # Trip reqeust link
     path('<slug:slug>/', views.TripDetail.as_view(), name='trip_detail'),  # Trip detail URL
     path('register/<slug:slug>', views.TripRegistration.as_view(), name='trip_registration'),  # Trip registration URL
 ]
