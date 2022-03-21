feeac31272ce3ee5d8cc13d75b08cbed91671f2b
Alex Ciobanu <tsopi10@gmail.com>
2022-03-21 20:20:04 +0200

    Add past_trips URL and link

--- trips/urls.py
+++ trips/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.TripList.as_view(), name='home'),  # Home URL
     path('dashboard/', views.TripsRegistered.as_view(), name='user_dashboard'),  # Trips history / user dashboard URL
-    path('dashboard/request/', views.TripRequest.as_view(), name='trip_request'),  # Trip reqeust link
+    path('dashboard/request/', views.TripRequest.as_view(), name='trip_request'),  # Trip reqeust URL
+    path('past_trips/', views.PastTrips.as_view(), name='past_trips'),  # Past trips URL
     path('<slug:slug>/', views.TripDetail.as_view(), name='trip_detail'),  # Trip detail URL
     path('register/<slug:slug>', views.TripRegistration.as_view(), name='trip_registration'),  # Trip registration URL
 ]
